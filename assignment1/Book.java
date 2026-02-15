b93db8d116ef37577dfefabc58fd070fbe3b5979
avipatel1226 <avipatel1226@gmail.com>
2026-02-15 11:19:19 -0500

    Initial commit - added Book class and LibraryManager structure

--- assignment1/Book.java
+++ assignment1/Book.java
@@ -34,22 +34,3 @@ public class Book {
         if(author != null && !author.trim().isEmpty()) this.author = author;
         else { this.author = "Unknown"; System.out.println("Invalid author"); }
     }
-
-    public String getIsbn() { return isbn; }
-    public void setIsbn(String isbn) {
-        if(isbn != null && (isbn.length() == 10 || isbn.length() == 13)) this.isbn = isbn;
-        else { this.isbn = "0000000000"; System.out.println("Invalid ISBN"); }
-    }
-
-    public boolean isAvailable() { return available; }
-    public void setAvailable(boolean available) { this.available = available; }
-
-    // Display book details
-    public void displayInfo() {
-        System.out.println("------------------------------");
-        System.out.println("Title: " + title);
-        System.out.println("Author: " + author);
-        System.out.println("ISBN: " + isbn);
-        System.out.println("Available: " + (available ? "Yes" : "No"));
-    }
-}
